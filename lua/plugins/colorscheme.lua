23773908980c5f5eb497446f8b310e1166beb33a
Vincent Kao <vincent0629@gmail.com>
2024-04-25 14:27:24 +0800

    Change background color to black.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,6 +2,14 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
-    opts = { style = "night" },
-  }
+    opts = {
+      style = "night",
+      styles = {
+        sidebars = "transparent",
+      },
+      on_colors = function(colors)
+        colors.bg = "#000000"
+      end,
+    },
+  },
 }
