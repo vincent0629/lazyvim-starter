cc31fef9f9d8b7490162ea1b8c0e847746224bb5
Vincent Kao <vincent0629@gmail.com>
2024-04-29 17:53:05 +0800

    Add key for reloading file with encoding.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,13 +4,20 @@
 local keymap = vim.keymap
 local mini = require("mini.bufremove")
 
-local disable = {"ft", "fT", "wd", "w-", "w|", "ww", "-", "`", "|", "bb", "K", "L", "ur", "uf", "uF", "us", "uw", "ul", "uL", "ud", "uc", "uh", "uT", "ub", "ui", "xl", "xq", "l", "cd", "gg", "gG", "gf"}
+local disable = {"ft", "fT", "wd", "w-", "w|", "ww", "-", "`", "|", "bb", "K", "L", "ur", "uf", "uF", "us", "uw", "ul", "uL", "ud", "uc", "uh", "uT", "ub", "ui", "xl", "xq", "l", "cd", "gg", "gG", "gf", "gb"}
 for i = 1, table.getn(disable) do
   keymap.set("n", "<leader>" .. disable[i], "")
 end
 disable = {"l", "f", "<tab>", "]", "d", "["}
 for i = 1, table.getn(disable) do
   keymap.set("n", "<leader><tab>" .. disable[i], "")
+end
+
+local enc_index = 0
+local ReloadWithEncoding = function()
+  enc_index = 1 - enc_index
+  local encodings = {"utf-8", "gbk"}
+  vim.cmd("e ++enc=" .. encodings[enc_index + 1])
 end
 
 keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New file" })
@@ -29,3 +36,5 @@ keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
 keymap.set("n", "<leader>gx", "<cmd>call jobstart([\"open\", expand(\"<cfile>\")])<cr>", { desc = "Open URL" })
 keymap.set("n", "<leader>gf", "<cmd>e <cfile><cr>", { desc = "Open file under cursor" })
+
+keymap.set("n", "<leader>ce", ReloadWithEncoding, { desc = "Reload with encoding" })
