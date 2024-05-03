01643ad6242bd1c5ad5ec0be6222f3234b3141d5
Vincent Kao <vincent0629@gmail.com>
2024-05-03 11:02:59 +0800

    Disable nvim-spectre.

---
+++ lua/plugins/disableplugin.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/flash.nvim",
+    enabled = false,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    enabled = false,    
+  },
+  {
+    "echasnovski/mini.pairs",
+    enabled = false,
+  },
+  {
+    "nvim-pack/nvim-spectre",
+    enabled = false,    
+  },
+}
