8d26108775e84bc9cc1d84a4c2d9751ff8af3983
Vincent Kao <vincent0629@gmail.com>
2024-05-15 12:58:45 +0800

    Remove unused <leader>s.

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,11 +1,20 @@
 return {
   {
     "folke/which-key.nvim",
-    opts = {
-      defaults = {
+    opts = function(_, opts)
+      opts.defaults = {
+        mode = { "n", "v" },
+        ["g"] = { name = "+goto" },
+        ["gs"] = { name = "+surround" },
+        ["z"] = { name = "+fold" },
+        ["]"] = { name = "+next" },
+        ["["] = { name = "+prev" },
+        ["<leader>c"] = { name = "+code" },
         ["<leader>f"] = { name = "+file" },
         ["<leader>g"] = { name = "+go to" },
-      },
-    },
+        ["<leader>q"] = { name = "+quit/session" },
+        ["<leader>w"] = { name = "+windows" },
+      }
+    end,
   }
 }
