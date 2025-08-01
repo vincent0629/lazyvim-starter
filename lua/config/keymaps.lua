ae2cfa35241a098b114890c25ca3c3f7044aaf8b
Vincent Kao <vincent0629@gmail.com>
2025-08-01 19:06:25 +0800

    Reset keymap.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local keymap = vim.keymap
-
-local disable = {"K", "L", "S", "-", ".", "?", "`", "|", "bb", "bd", "bD", "bo", "cd", "ft", "fT", "gB", "gY"}
-for i = 1, #disable do
-  keymap.del("n", "<leader>" .. disable[i])
-end
 
 local enc_index = 0
 local ReloadWithEncoding = function()
@@ -14,6 +8,17 @@ local ReloadWithEncoding = function()
   local encodings = {"utf-8", "GB18030"}
   vim.cmd("e ++enc=" .. encodings[enc_index + 1])
 end
+
+-- clear keymap
+vim.cmd.mapclear()
+
+-- set keymap
+local keymap = vim.keymap
+keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
+keymap.set("n", "<leader>n", "<cmd>lua LazyVim.pick('notifications')()<cr>", { desc = "Notification history" })
+
+keymap.set({"n", "v"}, "<leader>cf", "<cmd>lua LazyVim.format({force=true})<cr>", { desc = "Format" })
+keymap.set("n", "<leader>cs", "<cmd>sort<cr>", { desc = "Sort lines" })
 
 keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New file" })
 keymap.set("n", "<leader>ff", "<cmd>lua LazyVim.pick('files', {root=false})()<cr>", { desc = "Find file" })
@@ -25,11 +30,23 @@ keymap.set("n", "<leader>fx", "<cmd>lua LazyVim.pick('explorer', {layout='sideba
 keymap.set("n", "<leader>fr", "<cmd>lua LazyVim.pick('oldfiles', {cwd = vim.uv.cwd()})()<cr>", { desc = "Recent files" })
 keymap.set("n", "<leader>fl", ReloadWithEncoding, { desc = "Reload with encoding" })
 
+keymap.set("n", "<leader>ox", "<cmd>call jobstart([\"open\", expand(\"<cfile>\")])<cr>", { desc = "Open URL" })
+keymap.set("n", "<leader>of", "<cmd>e <cfile><cr>", { desc = "Open file under cursor" })
+
+keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
+
 keymap.set("n", "<leader>wc", "<cmd>close<cr>", { desc = "Close window" })
 keymap.set("n", "<leader>w|", "<cmd>vsplit<cr>", { desc = "Split window right" })
 keymap.set("n", "<leader>w-", "<cmd>split<cr>", { desc = "Split window below" })
 
-keymap.set("n", "<leader>ox", "<cmd>call jobstart([\"open\", expand(\"<cfile>\")])<cr>", { desc = "Open URL" })
-keymap.set("n", "<leader>of", "<cmd>e <cfile><cr>", { desc = "Open file under cursor" })
-
-keymap.set("n", "<leader>cs", "<cmd>sort<cr>", { desc = "Sort lines" })
+keymap.set("n", "<s-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
+keymap.set("n", "<s-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+keymap.set("n", "<c-t>", "<cmd>lua Snacks.terminal()<cr>", { desc = "Terminal" })
+keymap.set("n", "<c-h>", "<C-w>h", { desc = "Go to left window" })
+keymap.set("n", "<c-j>", "<C-w>j", { desc = "Go to lower window" })
+keymap.set("n", "<c-k>", "<C-w>k", { desc = "Go to upper window" })
+keymap.set("n", "<c-l>", "<C-w>l", { desc = "Go to right window" })
+keymap.set("n", "<c-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
+keymap.set("n", "<c-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
+keymap.set("n", "<c-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
+keymap.set("n", "<c-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
