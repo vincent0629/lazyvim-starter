6954c2e721fb5f740597d085052453c56441d89d
Vincent Kao <vincent0629@gmail.com>
2025-08-03 21:41:58 +0800

    Update keymap.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,24 +20,29 @@ keymap.set("n", "<leader>n", "<cmd>lua LazyVim.pick('notifications')()<cr>", { d
 keymap.set({"n", "v"}, "<leader>cf", "<cmd>lua LazyVim.format({force=true})<cr>", { desc = "Format" })
 keymap.set("n", "<leader>cs", "<cmd>sort<cr>", { desc = "Sort lines" })
 
-keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New file" })
 keymap.set("n", "<leader>ff", "<cmd>lua LazyVim.pick('files', {root=false})()<cr>", { desc = "Find file" })
 keymap.set("n", "<leader>fg", "<cmd>lua LazyVim.pick('live_grep', {root=false})()<cr>", { desc = "Grep file" })
 keymap.set("n", "<leader>fh", "<cmd>lua LazyVim.pick('resume')()<cr>", { desc = "Resume find/grep" })
 keymap.set("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save file" })
-keymap.set("n", "<leader>fc", "<cmd>lua Snacks.bufdelete()<cr>", { desc = "Close file" })
 keymap.set("n", "<leader>fx", "<cmd>lua LazyVim.pick('explorer', {layout='sidebar'})()<cr>", { desc = "Toggle file explorer" })
 keymap.set("n", "<leader>fr", "<cmd>lua LazyVim.pick('oldfiles', {cwd = vim.uv.cwd()})()<cr>", { desc = "Recent files" })
 keymap.set("n", "<leader>fl", ReloadWithEncoding, { desc = "Reload with encoding" })
+
+keymap.set("n", "<leader>gb", "<cmd>lua Snacks.picker.git_log_line()<cr>", { desc = "Git blame line" })
 
 keymap.set("n", "<leader>ox", "<cmd>call jobstart([\"open\", expand(\"<cfile>\")])<cr>", { desc = "Open URL" })
 keymap.set("n", "<leader>of", "<cmd>e <cfile><cr>", { desc = "Open file under cursor" })
 
 keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
 
+keymap.set("n", "<leader>ul", "<cmd>lua Snacks.toggle.line_number()<cr>", { desc = "Toggle line number" })
+
 keymap.set("n", "<leader>wc", "<cmd>close<cr>", { desc = "Close window" })
 keymap.set("n", "<leader>w|", "<cmd>vsplit<cr>", { desc = "Split window right" })
 keymap.set("n", "<leader>w-", "<cmd>split<cr>", { desc = "Split window below" })
+
+keymap.set("n", "<leader><tab>c", "<cmd>lua Snacks.bufdelete()<cr>", { desc = "Close tab" })
+keymap.set("n", "<leader><tab><tab>", "<cmd>tabnew<cr>", { desc = "New tab" })
 
 keymap.set("n", "<s-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
 keymap.set("n", "<s-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
