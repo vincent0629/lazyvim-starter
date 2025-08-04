51364b91aaff038852d08abe3657af5ecc10fe6e
Vincent Kao <vincent0629@gmail.com>
2025-08-04 10:44:42 +0800

    Disable unused plugins.

--- lua/plugins/disableplugin.lua
+++ lua/plugins/disableplugin.lua
@@ -7,4 +7,60 @@ return {
     "echasnovski/mini.pairs",
     enabled = false,
   },
+  {
+    "catppuccin/nvim",
+    enabled = false,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    enabled = false,
+  },
+  {
+    "rafamadriz/friendly-snippets",
+    enabled = false,
+  },
+  {
+    "stevearc/conform.nvim",
+    enabled = false,
+  },
+  {
+    "MagicDuck/grug-far.nvim",
+    enabled = false,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    enabled = false,
+  },
+  {
+    "nvim-lua/plenary.nvim",
+    enabled = false,
+  },
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    enabled = false,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = false,
+  },
+  {
+    "hrsh7th/cmp-buffer",
+    enabled = false,
+  },
+  {
+    "hrsh7th/cmp-path",
+    enabled = false,
+  },
+  {
+    "saadparwaiz1/cmp_luasnip",
+    enabled = false,
+  },
+  {
+    "folke/lazydev.nvim",
+    enabled = false,
+  },
+  {
+    "echasnovski/mini.ai",
+    enabled = false,
+  },
 }
