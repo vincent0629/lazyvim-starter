3c5bb34726163e68bf078ef347cebaa6251d65bb
Vincent Kao <vincent0629@gmail.com>
2026-01-19 17:06:05 +0800

    Remove nvim-treesitter and move which-key options to plugin.

--- init.lua
+++ init.lua
@@ -6,19 +6,6 @@ vim.cmd[[colorscheme tokyonight]]
 
 vim.notify = require('notify')
 
-require("nvim-treesitter.configs").setup({
-  ensure_installed = {"bash", "c", "cmake", "cpp", "css", "gn", "html", "java", "javascript", "json", "make", "python", "xml", "yaml"},
-  sync_install = false,
-})
-
-require("which-key").add({
-  {"<leader>c", group = "Code"},
-  {"<leader>f", group = "File"},
-  {"<leader>s", group = "Search"},
-  {"<leader>u", group = "UI"},
-  {"<leader>w", group = "Window"},
-})
-
 if vim.fn.argc() == 0 then
   require("persistence").load()
   vim.cmd("%argdel")
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,5 +2,14 @@ return {
   "folke/which-key.nvim",
   opts = {
     preset = "helix",
+    spec = {
+      {
+        {"<leader>c", group = "Code"},
+        {"<leader>f", group = "File"},
+        {"<leader>s", group = "Search"},
+        {"<leader>u", group = "UI"},
+        {"<leader>w", group = "Window"},
+      }
+    },
   },
 }
