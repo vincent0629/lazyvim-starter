0660d3cfc1df0a42a50590b6100e2545d592a8ca
Vincent Kao <vincent0629@gmail.com>
2026-01-22 09:48:07 +0800

    Default show ignored files.

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,12 +7,15 @@ return {
       sources = {
         files = {
           hidden = true,
+          ignored = true,
         },
         grep = {
           hidden = true,
+          ignored = true,
         },
         explorer = {
           hidden = true,
+          ignored = true,
           layout = {
             layout = {
               position = "right",
