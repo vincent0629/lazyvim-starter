01e3675961919a18e1d5360dd9d22f215bad947d
Vincent Kao <vincent0629@gmail.com>
2026-01-28 17:03:44 +0800

    Enable nvim-treesitter highlight.

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   branch = "master",
-  lazy = false,
+  main = "nvim-treesitter.configs",
   build = ":TSUpdate",
+  opts = {
+    ensure_installed = {"bash", "c", "cmake", "cpp", "css", "gn", "html", "java", "javascript", "json", "make", "python", "xml", "yaml"},
+    highlight = {
+      enable = true,
+    },
+  }
 }
